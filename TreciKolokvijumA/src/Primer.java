dcb4f0c10023efc51288e241deaa036d46290941
maksimT <maksim>
2018-01-17 17:43:59 +0100

    Obrisan komentar pre findLast

--- TreciKolokvijumA/src/Primer.java
+++ TreciKolokvijumA/src/Primer.java
@@ -1,18 +1,8 @@
 
 public class Primer {
-	/**
-	 * Find last index of element
-	 *
-	 * @param x
-	 *            array to search
-	 * @param y
-	 *            value to look for
-	 * @return last index of y in x; -1 if absent
-	 * @throws NullPointerException
-	 *             if x is null
-	 */
-	public int findLast(int[] x, int y) {
-		for (int i = x.length - 1; i > 0; i--) {
+
+	public int findFirst(int[] x, int y) {
+		for (int i =0; i<x.lenght-1; i++) {
 			if (x[i] == y) {
 				return y;
 			}
